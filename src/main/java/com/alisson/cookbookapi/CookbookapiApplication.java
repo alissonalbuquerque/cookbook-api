d75ea78a5aea86c55569d447e4b4c88aeecb7e20
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2021-10-05 23:12:35 -0300

    initial commit

---
+++ src/main/java/com/alisson/cookbookapi/CookbookapiApplication.java
@@ -0,0 +1,13 @@
+package com.alisson.cookbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alisson/cookbookapi/CookbookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisson.cookbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
